111aa59aa96c55d1cbf59cbdd900fa9c4bc3126e
Oleksandr Panichuk <oleksandr.panichuk.dev@gmail.com>
2026-01-06 16:04:18 +0200

    added clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
             attribute={"class"}
             defaultTheme={"dark"}
             enableSystem
-            disableTransitionOnChange
+            disableTransitionOnChang
           >
             {children}
 
